1c40aca736b9eac42d20d7f7057a8ef6bc1cd702
RPG3D <imxqliu@live.com>
2019-11-24 00:26:57 +0800

    add xmake script

---
+++ rpg3d/BuildWindows.bat
@@ -0,0 +1,10 @@
+
+xmake  f -p windows -a x64 -m release
+xmake -b
+
+xmake  f -p windows -a x64 -m debug
+xmake -b
+
+pause
+
+
---
+++ rpg3d/xmake.lua
@@ -0,0 +1,36 @@
+
+
+target("lfs")
+	set_kind("shared")
+
+	set_languages("c11", "cxx14")
+	
+	add_rules("mode.debug", "mode.release")
+	
+	if is_mode("debug") then
+		set_symbols("debug")
+		set_optimize("none")
+	else
+		set_optimize("fastest")
+	end
+	
+	add_includedirs("../../Lua")
+	
+	add_linkdirs("../../Lua/rpg3d/build/$(os)/$(arch)/$(mode)")
+	add_links("Lua")
+	
+	add_headerdirs("../src")
+	
+	add_files("../src/*.c")
+	
+	if (is_os("windows")) then
+		add_cxflags("/TC", {force = true})
+		add_defines("LUA_BUILD_AS_DLL=1")
+	end
+	
+	
+	if (is_os("android")) then
+		add_defines("ANDROID_STL=gnustl_shared")
+	end
+
+
